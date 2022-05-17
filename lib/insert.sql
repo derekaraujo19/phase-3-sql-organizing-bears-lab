07ea0eeff2959a315e323ba7fae29decf7297098
Derek Araujo <derekaraujo19@gmail.com>
2022-05-17 00:32:25 -0400

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,31 @@
+INSERT INTO
+  bears(name, age, sex, color, temperament, alive)
+  VALUES (null, 2, "male", "brown", null, 0);
+
+INSERT INTO
+  bears(name, age, sex, color, temperament, alive)
+  VALUES ("Mr. Chocolate", 10, "male", "brown", "mild", 1);
+
+INSERT INTO
+  bears(name, age, sex, color, temperament, alive)
+  VALUES ("Rowdy", 5, "male", "black", "aggressive", 1);
+
+INSERT INTO
+  bears(name, age, sex, color, temperament, alive)
+  VALUES ("Tabitha", 8, "female", "black", "mild", 1);
+
+INSERT INTO
+  bears(name, age, sex, color, temperament, alive)
+  VALUES ("Sergeant Brown", 12, "male", "brown", "ferocious", 1);
+
+INSERT INTO
+  bears(name, age, sex, color, temperament, alive)
+  VALUES ("Melissa", 4, "female", "brown", "curious", 1);
+
+INSERT INTO
+  bears(name, age, sex, color, temperament, alive)
+  VALUES ("Grinch", 20, "male", "brown", "mean", 1);
+
+INSERT INTO
+  bears(name, age, sex, color, temperament, alive)
+  VALUES ("Wendy", 7, "female", "black", "skittish", 1);
